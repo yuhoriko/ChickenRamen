37a7665283c0654634840b678a89e49d0b63fb40
yuhoriko <pon.de.yuri@gmail.com>
2019-05-19 17:47:53 +0900

    Initial Commit

---
+++ ChickenRamen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChickenRamen
+//
+//  Created by Yuriko Kimura on 2019/05/19.
+//  Copyright © 2019 Yuriko Kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
